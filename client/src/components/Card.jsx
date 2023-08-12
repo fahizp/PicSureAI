9a637835a22cb8394897f5fc2ddeaba5000cdb71
fahizp <fahizp3@gmail.com>
2023-08-12 15:17:15 +0530

    fix: the eslind issue

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 import { download } from '../assets';
 import { downloadImage } from '../utils';
 
-const Card = ({ _id, name, prompt, photo }) => (
+const Card = ({ _id, name, prompt, photo }) => (  //eslint-disable-line 
   <div className="rounded-xl group relative shadow-card hover:shadow-cardhover card">
     <img
       className="w-full h-auto object-cover rounded-xl"
