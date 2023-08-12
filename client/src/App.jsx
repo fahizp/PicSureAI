1a72968f394992942a784f99d5b3fcb02593067b
fahizp <fahizp3@gmail.com>
2023-08-12 15:16:17 +0530

    css:change the bg coloure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import { logo } from './assets';
 import { Home, CreatePost } from './pages';
@@ -7,17 +6,17 @@ const App = () => {
     <BrowserRouter>
       <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
         <Link to="/">
-          <img src={logo} alt="logo" className="w-28 object-contain" />
+          <img src={logo} alt="logo" className="w-36 object-contain" />
         </Link>
 
         <Link
           to="/create-post"
-          className="font-inter font-medium bg-[#6469ff] text-white px-4 py-2 rounded-md"
+          className="font-Space-Grotesk font-medium bg-[#33B89F] text-white px-4 py-2 rounded-md"
         >
           Create
         </Link>
       </header>
-      <main className="sm:p-8 px-4 py-8 w-full bg-[#f9f8fe] min-h-[calc(100vh-73)]">
+      <main className="sm:p-8 px-4 py-8 w-full bg-[#ffffff] min-h-[calc(100vh-73)]">
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/create-post" element={<CreatePost />}></Route>
