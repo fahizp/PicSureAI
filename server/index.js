4794c9a5afd6a274a850ac1c63d640930055b7ce
fahizp <fahizp3@gmail.com>
2023-09-09 14:46:08 +0530

    fix:add the cors for the vecel

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:["https://localhost:8080"],
+    methods:["POST", "GET" ],
+    credentials:true,
+}));
 app.use(express.json({limit:'50mb'}));
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', dalleRoutes);
